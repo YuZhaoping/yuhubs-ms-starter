60e90446d361797ca98c73803df031ab981efafb
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-02-19 12:36:20 -0500

    Add the BrandBar component

---
+++ front-end/src/components/BrandBar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+import clsx from 'clsx';
+
+
+import styled from '@material-ui/core/styles/styled';
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+}));
+
+
+const BrandIcon= styled('div')({
+  textAlign: 'center',
+  '& img': {
+    width: 24,
+    height: 24
+  }
+});
+
+const BrandName = styled('div')(({
+  theme
+}) => ({
+  whiteSpace: 'nowrap',
+  padding: theme.spacing(1, 4)
+}));
+
+
+const BrandBar = (props) => {
+  const { className } = props;
+
+  const classes = useStyles();
+
+  return (
+    <div className={clsx(classes.root, className)} >
+      <BrandIcon>
+        <img src={require("Assets/img/logo.png")} />
+      </BrandIcon>
+      <BrandName>Ecom App</BrandName>
+    </div>
+  );
+};
+
+
+export default BrandBar;
--- front-end/src/components/Sidebar/Sidebar.js
+++ front-end/src/components/Sidebar/Sidebar.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 
+import styled from '@material-ui/core/styles/styled';
+
 import Drawer from '@material-ui/core/Drawer';
 
-
 import Scrollbar from "react-double-scrollbar";
+
+
+import BrandBarBase from 'Components/BrandBar';
+
+
+const brandBarHeight = 48;
+
+const BrandBar = styled(BrandBarBase)(({
+  theme
+}) => ({
+  padding: theme.spacing(3, 2, 2, 6),
+
+  backgroundColor: theme.palette.primary.dark,
+  color: theme.palette.common.white,
+
+  height: `${brandBarHeight}px`
+}));
 
 
 const Sidebar = (props) => {
@@ -12,6 +30,8 @@ const Sidebar = (props) => {
 
   return (
     <Drawer {...rest} >
+      <BrandBar />
+
       <Scrollbar>
       </Scrollbar>
     </Drawer>
