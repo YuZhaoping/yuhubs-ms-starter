333f5e60fe56bc800cbf000531c1936a4c992943
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-02-21 20:08:26 -0500

    Add the PageTitleBar component

---
+++ front-end/src/pages/components/PageTitleBar/PageTitleBar.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import withStyles from '@material-ui/styles/withStyles';
+import styled from '@material-ui/core/styles/styled';
+
+import Typography from '@material-ui/core/Typography';
+
+
+import PageMenu from 'Pages/components/PageMenu';
+import PageTabs from 'Pages/components/PageTabs';
+
+
+const titleBarHeight = 36;
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    height: `${titleBarHeight}px`
+  }
+}));
+
+
+const Title = withStyles(theme => ({
+  root: {
+    marginRight: theme.spacing(12)
+  }
+}))(Typography);
+
+
+const BlackSpace = styled('div')({
+  flexGrow: 1
+});
+
+
+const PageTitleBar = (props) => {
+  const { title, menus, tabs, ...rest } = props;
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root} >
+      { title && <Title variant="h6">{title}</Title> }
+      <BlackSpace />
+      { tabs && <PageTabs tabs={tabs} { ...rest } /> }
+      { menus && <PageMenu menus={menus} />}
+    </div>
+  );
+};
+
+
+export default PageTitleBar;
