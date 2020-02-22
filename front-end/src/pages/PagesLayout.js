a050d2208c96b0964c8c8a26dddf9e22704782ca
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-02-21 20:09:40 -0500

    Add the PagesLayout

---
+++ front-end/src/pages/PagesLayout.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+import PageTitleBar from 'Pages/components/PageTitleBar';
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    overflow: 'hidden',
+    flexDirection: 'column'
+  },
+
+  pageHeader: {
+    backgroundColor: theme.palette.primary.dark,
+    color: theme.palette.common.white,
+    padding: theme.spacing(0, 2)
+  },
+
+  pageContent: {
+    flexGrow: 1,
+    marginLeft: theme.spacing(2)
+  }
+}));
+
+
+const PagesLayout = (props) => {
+  const { children, ...rest } = props;
+
+  const classes = useStyles();
+
+  return (
+    <div className={ classes.root } >
+      <div className={ classes.pageHeader }>
+        <PageTitleBar { ...rest } />
+      </div>
+      <div className={ classes.pageContent }>
+        { children }
+      </div>
+    </div>
+  );
+};
+
+
+export default PagesLayout;
