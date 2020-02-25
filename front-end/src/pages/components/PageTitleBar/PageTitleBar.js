e5fa10b2c997f798d1602d6802c44d2a09863411
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-02-25 12:38:37 -0500

    Refactor the PageTitleBar

--- front-end/src/pages/components/PageTitleBar/PageTitleBar.js
+++ front-end/src/pages/components/PageTitleBar/PageTitleBar.js
@@ -4,46 +4,15 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
 
-import makeStyles from '@material-ui/core/styles/makeStyles';
-import withStyles from '@material-ui/styles/withStyles';
-import styled from '@material-ui/core/styles/styled';
-
-import Typography from '@material-ui/core/Typography';
-
+import {
+  useStyles,
+  Title,
+  GrowSpace,
+  PlaceHold
+} from './components';
 
 import PageMenu from 'Pages/components/PageMenu';
 import PageTabs from 'Pages/components/PageTabs';
-
-
-import sizes from 'Components/sizes';
-
-const titleBarHeight = sizes.pageTitleBarHeight;
-
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    alignItems: 'center',
-    height: `${titleBarHeight}px`
-  }
-}));
-
-
-const Title = withStyles(theme => ({
-  root: {
-    marginRight: theme.spacing(12)
-  }
-}))(Typography);
-
-
-const GrowSpace = styled('div')({
-  flexGrow: 1
-});
-
-
-const PlaceHold = styled('div')({
-  width: '16px'
-});
 
 
 const PageTitleBar = (props) => {
---
+++ front-end/src/pages/components/PageTitleBar/components.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+
+import sizes from 'Components/sizes';
+
+const titleBarHeight = sizes.pageTitleBarHeight;
+
+
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import withStyles from '@material-ui/styles/withStyles';
+import styled from '@material-ui/core/styles/styled';
+
+import Typography from '@material-ui/core/Typography';
+
+
+export const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    height: `${titleBarHeight}px`
+  }
+}));
+
+
+export const Title = withStyles(theme => ({
+  root: {
+    marginRight: theme.spacing(12)
+  }
+}))(Typography);
+
+
+export const GrowSpace = styled('div')({
+  flexGrow: 1
+});
+
+
+export const PlaceHold = styled('div')({
+  width: '16px'
+});
