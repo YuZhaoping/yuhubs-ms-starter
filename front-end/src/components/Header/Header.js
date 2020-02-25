302c35cda47010984facd250025b772c38b070e3
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-02-25 09:32:16 -0500

    Refactor the Header

--- front-end/src/components/Header/Header.js
+++ front-end/src/components/Header/Header.js
@@ -1,61 +1,25 @@
 import React from 'react';
 
 
-import styled from '@material-ui/core/styles/styled';
-
-import MuiAppBar from '@material-ui/core/AppBar';
-import MuiIconButton from '@material-ui/core/IconButton';
-
-import MenuIcon from '@material-ui/icons/Menu';
-
+import {
+  AppBar,
+  Toolbar,
+  SidebarButton,
+  BrandBar,
+  GrowSpace,
+  PlaceHold
+} from './components';
 
 import AppsMenu from 'Components/AppsMenu';
 import LanguageSwitcher from 'Components/LanguageSwitcher';
 
-import BrandBarBase from 'Components/BrandBar';
 
+import MenuIcon from '@material-ui/icons/Menu';
 
-import sizes from 'Components/sizes';
-
-const toolbarHeight = sizes.headerToolbarHeight;
-
-
-const AppBar = styled(MuiAppBar)(({
-  theme
-}) => ({
-  top: 0,
-  background: theme.palette.common.white,
-  color: theme.menubar.dark
-}));
-
-
-const Toolbar = styled('div')({
-  position: 'relative',
-  display: 'flex',
-  alignItems: 'center',
-
-  height: `${toolbarHeight}px`
-});
-
-
-const GrowSpace = styled('div')({
-  flexGrow: 1,
-  width: '100%'
-});
-
-const PlaceHold = styled('div')({
-  width: '16px'
-});
-
-
-const SidebarButton = styled(MuiIconButton)(({
-  theme
-}) => ({
-  padding: theme.spacing(2)
-}));
 
 const AuthedToolbar = ({ onDrawerToggle, authUser }) => (
   <Toolbar>
+
     <SidebarButton
       onClick={ onDrawerToggle }
       color="inherit"
@@ -71,20 +35,14 @@ const AuthedToolbar = ({ onDrawerToggle, authUser }) => (
     <LanguageSwitcher />
 
     <PlaceHold />
+
   </Toolbar>
 );
 
 
-const BrandBar = styled(BrandBarBase)(({
-  theme
-}) => ({
-  borderRadius: '4px',
-  margin: theme.spacing(0, 4),
-  padding: theme.spacing(1, 2, 0, 2)
-}));
-
 const UnauthedToolbar = () => (
   <Toolbar>
+
     <BrandBar />
 
     <GrowSpace />
@@ -92,6 +50,7 @@ const UnauthedToolbar = () => (
     <LanguageSwitcher />
 
     <PlaceHold />
+
   </Toolbar>
 );
 
---
+++ front-end/src/components/Header/components.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+
+import sizes from 'Components/sizes';
+
+const toolbarHeight = sizes.headerToolbarHeight;
+
+
+import styled from '@material-ui/core/styles/styled';
+
+import MuiAppBar from '@material-ui/core/AppBar';
+import MuiIconButton from '@material-ui/core/IconButton';
+
+
+import BrandBarBase from 'Components/BrandBar';
+
+
+export const AppBar = styled(MuiAppBar)(({
+  theme
+}) => ({
+  top: 0,
+  background: theme.palette.common.white,
+  color: theme.menubar.dark
+}));
+
+
+export const Toolbar = styled('div')({
+  position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
+
+  height: `${toolbarHeight}px`
+});
+
+
+export const SidebarButton = styled(MuiIconButton)(({
+  theme
+}) => ({
+  padding: theme.spacing(2)
+}));
+
+
+export const BrandBar = styled(BrandBarBase)(({
+  theme
+}) => ({
+  borderRadius: '4px',
+  margin: theme.spacing(0, 4),
+  padding: theme.spacing(1, 2, 0, 2)
+}));
+
+
+export const GrowSpace = styled('div')({
+  flexGrow: 1,
+  width: '100%'
+});
+
+
+export const PlaceHold = styled('div')({
+  width: '16px'
+});
