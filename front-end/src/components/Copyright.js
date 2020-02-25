d5e66a2eacab7c5d5dfc8385c61904423a6369e8
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-02-25 06:16:33 -0500

    Add the Copyright component

---
+++ front-end/src/components/Copyright.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
+
+const Copyright = () => (
+  <Typography variant="body2" color="textSecondary" align="center">
+    {'Copyright © '}
+    <Link color="inherit" href="#">
+      Zhaoping Yu
+    </Link>
+    {' '}
+    {new Date().getFullYear()}
+    {'.'}
+  </Typography>
+);
+
+
+export default Copyright;
