28a8eefd6ae94a85583cda8c010845574e650307
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-02-28 20:00:22 -0500

    Add start for test

---
+++ commons/web/src/test/java/com/yuhubs/ms/web/WebConfiguredTestBase.java
@@ -0,0 +1,14 @@
+package com.yuhubs.ms.web;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@ContextConfiguration(classes = WebTestConfig.class)
+@WebAppConfiguration
+public class WebConfiguredTestBase {
+}
---
+++ commons/web/src/test/java/com/yuhubs/ms/web/WebTestConfig.java
@@ -0,0 +1,7 @@
+package com.yuhubs.ms.web;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebTestConfig {
+}
