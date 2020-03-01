3dbb328dce071fa0104d9215b940bd5fad8aa1a2
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-03-01 11:33:17 -0500

    Configure the http security with jwt auth filter

--- auth/mvc/src/main/java/com/yuhubs/ms/security/web/SecurityConfigurationSupport.java
+++ auth/mvc/src/main/java/com/yuhubs/ms/security/web/SecurityConfigurationSupport.java
@@ -2,15 +2,22 @@ package com.yuhubs.ms.security.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yuhubs.ms.security.JwtTokenServiceContext;
+import com.yuhubs.ms.security.web.token.JwtAuthenticationFilter;
+import com.yuhubs.ms.security.web.token.JwtAuthenticationProvider;
 import com.yuhubs.ms.web.JsonMapperBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @EnableWebSecurity
 @Configuration
@@ -55,6 +62,52 @@ public class SecurityConfigurationSupport extends WebSecurityConfigurerAdapter {
 	@Bean
 	public AuthenticationFailureHandler authenticationFailureHandler(SecurityHandlerSupplier supplier) {
 		return supplier.authenticationFailureHandler();
+	}
+
+
+	@Override
+	protected final void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(new JwtAuthenticationProvider());
+		configureAuthenticationManager(auth);
+	}
+
+	protected void configureAuthenticationManager(AuthenticationManagerBuilder auth)
+			throws Exception {
+	}
+
+
+	@Override
+	protected final void configure(HttpSecurity http) throws Exception {
+		final SecurityHandlerSupplier supplier = this.handlerSupplier;
+
+		http.csrf().disable()
+				.exceptionHandling()
+				.authenticationEntryPoint(supplier.unauthorizedEntryPoint())
+				.accessDeniedHandler(supplier.accessDeniedHandler())
+				.and()
+				.sessionManagement().sessionCreationPolicy(STATELESS);
+
+		configureRequestAuthorization(http);
+
+		// JwtAuthenticationFilter must precede LogoutFilter,
+		// otherwise LogoutHandler wouldn't know who logs out.
+		http.addFilterBefore(newJwtAuthenticationFilter(), LogoutFilter.class);
+
+		configureFilters(http);
+	}
+
+	private final JwtAuthenticationFilter newJwtAuthenticationFilter() {
+		return new JwtAuthenticationFilter(this.handlerSupplier);
+	}
+
+
+	protected void configureRequestAuthorization(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/").permitAll()
+				.anyRequest().authenticated();
+	}
+
+	protected void configureFilters(HttpSecurity http) throws Exception {
 	}
 
 
