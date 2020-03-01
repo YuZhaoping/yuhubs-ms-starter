56171c25979e0a288de090d50de627dd81eae7fe
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-03-01 10:12:24 -0500

    Add the SecurityHandlerSupplier

--- auth/mvc/src/main/java/com/yuhubs/ms/security/web/SecurityConfigurationSupport.java
+++ auth/mvc/src/main/java/com/yuhubs/ms/security/web/SecurityConfigurationSupport.java
@@ -1,6 +1,8 @@
 package com.yuhubs.ms.security.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yuhubs.ms.security.JwtTokenServiceContext;
+import com.yuhubs.ms.web.JsonMapperBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,12 +12,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfigurationSupport extends WebSecurityConfigurerAdapter {
 
-
 	protected final JwtTokenServiceContext jwtTokenServiceContext;
+
+	protected final SecurityHandlerSupplier handlerSupplier;
 
 
 	public SecurityConfigurationSupport() {
 		this.jwtTokenServiceContext = new JwtTokenServiceContext();
+		this.handlerSupplier = createSecurityHandlerSupplier();
 	}
 
 
@@ -24,9 +28,27 @@ public class SecurityConfigurationSupport extends WebSecurityConfigurerAdapter {
 		return this.jwtTokenServiceContext;
 	}
 
+	@Bean
+	public SecurityHandlerSupplier securityHandlerSupplier() {
+		return this.handlerSupplier;
+	}
+
 
 	protected final JwtTokenServiceContext getJwtTokenServiceContext() {
 		return this.jwtTokenServiceContext;
+	}
+
+
+	protected SecurityHandlerSupplier createSecurityHandlerSupplier(ObjectMapper objectMapper) {
+		return new SecurityHandlerSupplier(this, objectMapper);
+	}
+
+	protected ObjectMapper createObjectMapper() {
+		return JsonMapperBuilder.buildJacksonMapper();
+	}
+
+	private final SecurityHandlerSupplier createSecurityHandlerSupplier() {
+		return createSecurityHandlerSupplier(createObjectMapper());
 	}
 
 }
---
+++ auth/mvc/src/main/java/com/yuhubs/ms/security/web/SecurityHandlerSupplier.java
@@ -0,0 +1,40 @@
+package com.yuhubs.ms.security.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yuhubs.ms.security.JwtTokenServiceContext;
+import com.yuhubs.ms.security.SecurityProperties;
+import com.yuhubs.ms.security.token.JwtTokenService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityHandlerSupplier {
+
+	protected final SecurityConfigurationSupport support;
+
+	protected final ObjectMapper objectMapper;
+
+
+	public SecurityHandlerSupplier(SecurityConfigurationSupport support,
+								   ObjectMapper objectMapper) {
+		this.support = support;
+		this.objectMapper = objectMapper;
+	}
+
+
+	public final ObjectMapper objectMapper() {
+		return this.objectMapper;
+	}
+
+	public final JwtTokenServiceContext jwtTokenServiceContext() {
+		return this.support.getJwtTokenServiceContext();
+	}
+
+	public final SecurityProperties securityProperties() {
+		return jwtTokenServiceContext().getSecurityProperties();
+	}
+
+	public final JwtTokenService jwtTokenService() {
+		return jwtTokenServiceContext().getJwtTokenService();
+	}
+
+}
