6d68ff28ba447100fba57534ab616fb142d3953a
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-03-02 06:57:44 -0500

    Add the JwtTokenServiceContext

---
+++ auth/core/src/main/java/com/yuhubs/ms/security/JwtTokenServiceContext.java
@@ -0,0 +1,49 @@
+package com.yuhubs.ms.security;
+
+import com.yuhubs.ms.security.token.DefaultJwtTokenService;
+import com.yuhubs.ms.security.token.JwtTokenService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JwtTokenServiceContext {
+
+	protected final SecurityProperties securityProperties;
+
+	protected JwtTokenService jwtTokenService;
+
+
+	public JwtTokenServiceContext() {
+		this.securityProperties = new SecurityProperties();
+	}
+
+
+	protected JwtTokenService createJwtTokenService(SecurityProperties properties) {
+		return new DefaultJwtTokenService(properties);
+	}
+
+
+	@Bean
+	public SecurityProperties securityProperties() {
+		return this.securityProperties;
+	}
+
+	@Bean
+	public JwtTokenService jwtTokenService(SecurityProperties properties) {
+		// lazy create for security properties
+		if (this.jwtTokenService == null) {
+			this.jwtTokenService = createJwtTokenService(properties);
+		}
+		return this.jwtTokenService;
+	}
+
+
+	public SecurityProperties getSecurityProperties() {
+		return this.securityProperties;
+	}
+
+	public JwtTokenService getJwtTokenService() {
+		return this.jwtTokenService;
+	}
+
+}
