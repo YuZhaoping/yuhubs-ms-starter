9ccbf0682ea9f229fb863135717f3f5a54b5777f
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-03-09 09:20:01 -0400

    Add the MockAuthUserService

---
+++ auth/mvc/src/main/java/com/yuhubs/ms/auth/mock/MockAuthUserService.java
@@ -0,0 +1,42 @@
+package com.yuhubs.ms.auth.mock;
+
+import com.yuhubs.ms.security.auth.AuthUser;
+import com.yuhubs.ms.security.auth.AuthUserService;
+import com.yuhubs.ms.security.auth.SignUpRequest;
+import com.yuhubs.ms.security.auth.exceptions.UserAlreadyExistsException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service("AuthUserService")
+public class MockAuthUserService implements AuthUserService {
+
+	private final MockUserManager userManager;
+
+
+	public MockAuthUserService(MockUserManager userManager) {
+		this.userManager = userManager;
+	}
+
+
+	@Override
+	public AuthUser signUpUser(SignUpRequest request) throws UserAlreadyExistsException {
+		return this.userManager.signUpUser(request);
+	}
+
+	@Override
+	public Optional<AuthUser> getUserById(Long userId) {
+		return this.userManager.getUserById(userId);
+	}
+
+	@Override
+	public Optional<AuthUser> getUserByName(String username) {
+		return this.userManager.getUserByName(username);
+	}
+
+
+	public MockUserManager mockUserManager() {
+		return this.userManager;
+	}
+
+}
