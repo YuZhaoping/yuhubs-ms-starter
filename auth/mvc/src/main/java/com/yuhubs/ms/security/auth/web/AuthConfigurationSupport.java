d79bd4e0f97fa80e4ada578a2175a2459ce313f2
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-03-09 10:37:25 -0400

    Config auth endpoints

--- auth/mvc/src/main/java/com/yuhubs/ms/security/auth/web/AuthConfigurationSupport.java
+++ auth/mvc/src/main/java/com/yuhubs/ms/security/auth/web/AuthConfigurationSupport.java
@@ -4,11 +4,23 @@ import com.yuhubs.ms.security.web.SecurityConfigurationSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
 
 @EnableWebSecurity
 @Configuration
 public class AuthConfigurationSupport extends SecurityConfigurationSupport implements AuthApiEndpoints {
+
+	private static final String SIGNOUT_SUCCESS_URL = SIGNIN_ENDPOINT + "?logout";
+
+	private static final String VERIFY_EMAIL_PATTERN =
+			SIGNUP_ENDPOINT + "/.*/verify_email/.*";
+
+	private static final String RESET_PASSWORD_PATTERN =
+			SIGNUP_ENDPOINT + "/.*/reset_password/.*";
 
 
 	protected final AuthWebSecurityContext context;
@@ -31,5 +43,24 @@ public class AuthConfigurationSupport extends SecurityConfigurationSupport imple
 	public AuthWebSecurityContext authWebSecurityContext() {
 		return this.context;
 	}
+
+
+	@Override
+	protected void configureRequestAuthorization(HttpSecurity http) throws Exception {
+		http.logout()
+				.logoutUrl(SIGNOUT_ENDPOINT)
+				.logoutSuccessUrl(SIGNOUT_SUCCESS_URL)
+				.permitAll();
+
+		http.authorizeRequests()
+				.antMatchers(POST, SIGNIN_ENDPOINT).permitAll()
+				.antMatchers(POST, SIGNUP_ENDPOINT).permitAll()
+				.antMatchers(PUT, RESET_PASSWORD_ENDPOINT).permitAll()
+				.regexMatchers(VERIFY_EMAIL_PATTERN).permitAll()
+				.regexMatchers(RESET_PASSWORD_PATTERN).permitAll();
+
+		super.configureRequestAuthorization(http);
+	}
+
 
 }
