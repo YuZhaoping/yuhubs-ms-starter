8b4eeaa7dc036f156df79306a59f0be0fa17c9ce
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-03-10 09:02:47 -0400

    Add the AuthServiceBase

---
+++ auth/mvc/src/main/java/com/yuhubs/ms/security/auth/service/AuthServiceBase.java
@@ -0,0 +1,48 @@
+package com.yuhubs.ms.security.auth.service;
+
+import com.yuhubs.ms.security.auth.AuthSecurityContext;
+import com.yuhubs.ms.security.auth.AuthUser;
+import com.yuhubs.ms.security.auth.AuthUserService;
+import com.yuhubs.ms.security.auth.event.AuthConfirmUrlsBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public abstract class AuthServiceBase {
+
+	protected final AuthServiceSupplier supplier;
+
+
+	public AuthServiceBase(AuthServiceSupplier supplier) {
+		this.supplier = supplier;
+	}
+
+
+	protected final AuthUser getUserByToken(String token) throws AuthenticationException {
+		Authentication authentication = authSecurityContext().jwtTokenService().parseJwtToken(token);
+
+		Long userId = (Long) authentication.getPrincipal();
+
+		Optional<AuthUser> userOp = authUserService().getUserById(userId);
+		if (!userOp.isPresent()) {
+			throw new UsernameNotFoundException("Invalid user id");
+		}
+
+		return userOp.get();
+	}
+
+	protected final AuthSecurityContext authSecurityContext() {
+		return this.supplier.authSecurityContext();
+	}
+
+	protected final AuthUserService authUserService() {
+		return this.supplier.authUserService();
+	}
+
+	protected final AuthConfirmUrlsBuilder authConfirmUrlsBuilder() {
+		return this.supplier.authConfirmUrlsBuilder();
+	}
+
+}
