9f146cd424be6df29934508790070852590caa9c
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-03-11 10:20:25 -0400

    Config mock users

---
+++ services/catalog/src/main/java/com/yuhubs/ecom/catalog/app/MockUserConfig.java
@@ -0,0 +1,22 @@
+package com.yuhubs.ecom.catalog.app;
+
+import com.yuhubs.ms.auth.mock.MockAuthUserService;
+import com.yuhubs.ms.auth.mock.MockUserManager;
+import com.yuhubs.ms.auth.mock.config.MockUserConfigSupport;
+import com.yuhubs.ms.config.YamlPropertyLoaderFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource(value = {"classpath:mock-users.yml"}, factory = YamlPropertyLoaderFactory.class)
+@ConfigurationProperties(prefix = "mock")
+public class MockUserConfig extends MockUserConfigSupport {
+
+	@Bean
+	public MockAuthUserService authUserService() {
+		return new MockAuthUserService(new MockUserManager(this));
+	}
+
+}
