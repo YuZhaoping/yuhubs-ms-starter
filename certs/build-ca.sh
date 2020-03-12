1560a6d19d48e0ef116308813e7c63e9f35d644e
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-03-12 11:29:11 -0400

    Polish

--- certs/build-ca.sh
+++ certs/build-ca.sh
@@ -13,31 +13,39 @@ export PREFIX=yuhubs
 
 # Build the CAs
 if [ ! -f "$OUTPUT_CA_DIR/$PREFIX-ecom.pem" ]; then
+
 # Build Root CA
 echo "--- Build Root CA ---"
-cfssl gencert -initca ./ca/ca-csr.json | cfssljson -bare $OUTPUT_CA_DIR/$PREFIX-ca
+cfssl gencert -initca ./ca/ca-csr.json | \
+cfssljson -bare $OUTPUT_CA_DIR/$PREFIX-ca
 
 
 # Build Ecom CA (intermediate)
 echo "--- Build Ecom CA (intermediate) ---"
-cfssl gencert -initca ./ca/ecom-csr.json | cfssljson -bare $OUTPUT_CA_DIR/$PREFIX-ecom
+cfssl gencert -initca ./ca/ecom-csr.json | \
+cfssljson -bare $OUTPUT_CA_DIR/$PREFIX-ecom
 
 cfssl sign -ca=$OUTPUT_CA_DIR/$PREFIX-ca.pem -ca-key=$OUTPUT_CA_DIR/$PREFIX-ca-key.pem \
   -config=./ca/ca-config.json -profile="intermediate_ca" \
-  $OUTPUT_CA_DIR/$PREFIX-ecom.csr | cfssljson -bare $OUTPUT_CA_DIR/$PREFIX-ecom
+  $OUTPUT_CA_DIR/$PREFIX-ecom.csr | \
+cfssljson -bare $OUTPUT_CA_DIR/$PREFIX-ecom
+
 fi
 
 # Verify the Ecom CA
 echo "--- Verify the Ecom CA ---"
-openssl verify -CAfile $OUTPUT_CA_DIR/$PREFIX-ca.pem $OUTPUT_CA_DIR/$PREFIX-ecom.pem
+openssl verify -CAfile $OUTPUT_CA_DIR/$PREFIX-ca.pem \
+  $OUTPUT_CA_DIR/$PREFIX-ecom.pem
 
 
 # Bundle the Root & Ecom CAs
 if [ ! -f "$OUTPUT_DIR/$PREFIX-ecom-ca.crt" ]; then
 echo "--- Bundle the Root & Ecom CAs ---"
-mkbundle -f $OUTPUT_DIR/$PREFIX-ecom-ca.crt $OUTPUT_CA_DIR/$PREFIX-ca.pem $OUTPUT_CA_DIR/$PREFIX-ecom.pem
+mkbundle -f $OUTPUT_DIR/$PREFIX-ecom-ca.crt \
+  $OUTPUT_CA_DIR/$PREFIX-ca.pem $OUTPUT_CA_DIR/$PREFIX-ecom.pem
 fi
 
 # Verify the bundled CAs
 echo "--- Verify the bundled CAs ---"
-openssl verify -CAfile $OUTPUT_DIR/yuhubs-ecom-ca.crt $OUTPUT_CA_DIR/$PREFIX-ecom.pem
+openssl verify -CAfile $OUTPUT_DIR/yuhubs-ecom-ca.crt \
+  $OUTPUT_CA_DIR/$PREFIX-ecom.pem
