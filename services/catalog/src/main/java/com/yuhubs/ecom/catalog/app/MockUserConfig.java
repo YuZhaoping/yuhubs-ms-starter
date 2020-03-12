f55e8bedbc244e50a18a66502b2fb1503b5d81f7
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-03-12 07:06:52 -0400

    Setup the InitMockUsersTask

--- services/catalog/src/main/java/com/yuhubs/ecom/catalog/app/MockUserConfig.java
+++ services/catalog/src/main/java/com/yuhubs/ecom/catalog/app/MockUserConfig.java
@@ -4,15 +4,37 @@ import com.yuhubs.ms.auth.mock.MockAuthUserService;
 import com.yuhubs.ms.auth.mock.MockUserManager;
 import com.yuhubs.ms.auth.mock.config.MockUserConfigSupport;
 import com.yuhubs.ms.config.YamlPropertyLoaderFactory;
+import com.yuhubs.ms.security.auth.web.AuthWebSecurityContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 @Configuration
 @PropertySource(value = {"classpath:mock-users.yml"}, factory = YamlPropertyLoaderFactory.class)
 @ConfigurationProperties(prefix = "mock")
 public class MockUserConfig extends MockUserConfigSupport {
+
+	@Component
+	public static class InitMockUsersTask implements CommandLineRunner {
+
+		@Autowired
+		private MockAuthUserService authUserService;
+
+		@Autowired
+		private AuthWebSecurityContext authSecurityContext;
+
+
+		@Override
+		public void run(String... args) throws Exception {
+			MockUserManager mockUserManager = authUserService.mockUserManager();
+			mockUserManager.initUsers(authSecurityContext);
+		}
+
+	}
 
 	@Bean
 	public MockAuthUserService authUserService() {
