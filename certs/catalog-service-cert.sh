6b8eef82c3ab85d57abffd21959618c39f2bcad7
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-03-13 10:06:19 -0400

    Generate catalog service's keystore.p12

--- certs/catalog-service-cert.sh
+++ certs/catalog-service-cert.sh
@@ -9,6 +9,8 @@ export OUTPUT_CA_DIR=$OUTPUT_DIR/ca
 export CA_NAME=yuhubs-ecom
 export HOST_NAME=catalog-service
 
+export PASSWD=yuhubs123
+
 
 export OUTPUT_HOST_DIR=$OUTPUT_DIR/$HOST_NAME
 
@@ -35,3 +37,29 @@ fi
 echo "--- Verify the Server Cert ---"
 openssl verify -CAfile $OUTPUT_DIR/$CA_NAME-ca.crt \
   $OUTPUT_HOST_DIR/$HOST_NAME.pem
+
+
+# Generate pkcs12 file
+if [ ! -f "$OUTPUT_HOST_DIR/$HOST_NAME.p12" ]; then
+echo "--- Generate pkcs12 file ---"
+openssl pkcs12 -export \
+  -in $OUTPUT_HOST_DIR/$HOST_NAME.pem \
+  -inkey $OUTPUT_HOST_DIR/$HOST_NAME-key.pem \
+  -out $OUTPUT_HOST_DIR/$HOST_NAME.p12 \
+  -name $HOST_NAME \
+  -password pass:$PASSWD
+fi
+
+
+# Generate keystore
+if [ ! -f "$OUTPUT_HOST_DIR/keystore.p12" ]; then
+echo "--- Generate keystore ---"
+keytool -importkeystore \
+  -srckeystore $OUTPUT_HOST_DIR/$HOST_NAME.p12 \
+  -srcstoretype PKCS12 \
+  -srcstorepass $PASSWD \
+  -destkeystore $OUTPUT_HOST_DIR/keystore.p12 \
+  -deststoretype PKCS12 \
+  -deststorepass $PASSWD\
+  -destkeypass $PASSWD
+fi
