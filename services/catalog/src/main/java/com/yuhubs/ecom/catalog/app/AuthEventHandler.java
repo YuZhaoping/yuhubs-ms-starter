c06084cfdca5d3b7c2d830032f8b879601cf3093
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-03-17 18:07:27 -0400

    Add the AuthEventHandler

---
+++ services/catalog/src/main/java/com/yuhubs/ecom/catalog/app/AuthEventHandler.java
@@ -0,0 +1,54 @@
+package com.yuhubs.ecom.catalog.app;
+
+import com.yuhubs.ms.auth.mail.AuthEmailSender;
+import com.yuhubs.ms.mail.MailTemplateSupport;
+import com.yuhubs.ms.security.auth.AuthEventSupport;
+import com.yuhubs.ms.security.auth.event.data.ConfirmEmailData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.mail.MailException;
+
+@Configuration
+@Profile("dev")
+public class AuthEventHandler extends AuthEventSupport {
+
+	private static Logger LOG = LoggerFactory.getLogger("yuhubs.ms.auth.mail");
+
+
+	private final AuthEmailSender emailSender;
+
+	@Autowired
+	private MailTemplateSupport mailTemplateSupport;
+
+
+	public AuthEventHandler() {
+		this.emailSender = new AuthEmailSender();
+	}
+
+
+	@Override
+	protected void handleConfirmEmail(ConfirmEmailData eventData) {
+		String templateId;
+
+		switch (eventData.getType()) {
+			case RESET_PASSWORD:
+				templateId = AuthTemplates.RESET_PASSWORD_ID;
+				break;
+			case VERIFY_EMAIL:
+			default:
+				templateId = AuthTemplates.VERIFY_EMAIL_ID;
+				break;
+		}
+
+		try {
+			this.emailSender.sendEmail(this.mailTemplateSupport,
+					templateId, eventData);
+		} catch (MailException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
