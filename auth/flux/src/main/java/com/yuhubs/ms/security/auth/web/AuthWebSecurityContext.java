f2b6c8a3d370aadc792258eb26e2addc5f3bc641
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-04-01 08:57:50 -0400

    Add the AuthWebSecurityContext

---
+++ auth/flux/src/main/java/com/yuhubs/ms/security/auth/web/AuthWebSecurityContext.java
@@ -0,0 +1,26 @@
+package com.yuhubs.ms.security.auth.web;
+
+import com.yuhubs.ms.security.auth.AuthSecurityContext;
+import com.yuhubs.ms.security.auth.AuthUserService;
+
+public class AuthWebSecurityContext extends AuthSecurityContext implements AuthUserService.Provider {
+
+	private final AuthConfigurationSupport support;
+
+
+	AuthWebSecurityContext(AuthConfigurationSupport support) {
+		super(support.getJwtTokenServiceContext());
+		this.support = support;
+	}
+
+
+	public AuthUserService.Provider userServiceProvider() {
+		return this;
+	}
+
+	@Override
+	public AuthUserService authUserService() {
+		return this.support.getAuthUserService();
+	}
+
+}
