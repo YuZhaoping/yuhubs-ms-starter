b1c39760b0e4fca24e157f88dfe5f22d526da4a3
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-04-03 12:00:45 -0400

    Setup login filter

--- auth/flux/src/main/java/com/yuhubs/ms/security/auth/web/AuthConfigurationSupport.java
+++ auth/flux/src/main/java/com/yuhubs/ms/security/auth/web/AuthConfigurationSupport.java
@@ -2,11 +2,18 @@ package com.yuhubs.ms.security.auth.web;
 
 import com.yuhubs.ms.security.auth.AuthUserService;
 import com.yuhubs.ms.security.auth.details.AuthUserDetailsService;
+import com.yuhubs.ms.security.auth.web.login.LoginAuthenticationConverter;
+import com.yuhubs.ms.security.auth.web.login.LoginAuthenticationManager;
 import com.yuhubs.ms.security.web.SecurityConfigurationSupport;
+import com.yuhubs.ms.security.web.SecurityHandlerSupplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 
 @EnableWebFluxSecurity
 @Configuration
@@ -43,11 +50,54 @@ public abstract class AuthConfigurationSupport extends SecurityConfigurationSupp
 	}
 
 
+	@Override
+	protected void configureFilters(ServerHttpSecurity http) {
+		super.configureFilters(http);
+		setupLoginFilter(http);
+	}
+
+
 	protected abstract AuthUserService getAuthUserService();
 
 
 	protected ReactiveUserDetailsService createUserDetailsService() {
 		return new AuthUserDetailsService(this.context.userServiceProvider());
+	}
+
+
+	protected final void setupLoginFilter(ServerHttpSecurity http) {
+		AuthenticationWebFilter loginFilter = createLoginFilter();
+
+		http.addFilterAt(loginFilter, SecurityWebFiltersOrder.FORM_LOGIN);
+	}
+
+	private final AuthenticationWebFilter createLoginFilter() {
+		final SecurityHandlerSupplier supplier = this.handlerSupplier;
+
+		final LoginAuthenticationManager authenticationManager =
+				new LoginAuthenticationManager(this.context, this.userDetailsService);
+
+		final AuthenticationWebFilter loginFilter =
+				new AuthenticationWebFilter(authenticationManager);
+
+		final LoginAuthenticationConverter authenticationConverter =
+				new LoginAuthenticationConverter(supplier.objectMapper());
+
+		loginFilter.setServerAuthenticationConverter(authenticationConverter);
+
+		loginFilter.setRequiresAuthenticationMatcher(
+				ServerWebExchangeMatchers.pathMatchers(SIGNIN_ENDPOINT)
+		);
+
+		loginFilter.setAuthenticationSuccessHandler(
+				supplier.authenticationSuccessHandler()
+		);
+
+		loginFilter.setAuthenticationFailureHandler(
+				supplier.authenticationFailureHandler()
+		);
+
+		return loginFilter;
 	}
 
 }
