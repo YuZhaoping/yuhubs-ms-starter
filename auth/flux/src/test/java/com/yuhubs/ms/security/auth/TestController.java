62a1a635f69ccb27d0dfc8404cb65cff336ed41d
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-04-06 13:09:06 -0400

    Add the TestController

---
+++ auth/flux/src/test/java/com/yuhubs/ms/security/auth/TestController.java
@@ -0,0 +1,24 @@
+package com.yuhubs.ms.security.auth;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public Mono<Void> handleRoot() {
+		return Mono.empty();
+	}
+
+	@GetMapping("/test")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public Mono<Void> handleTest() {
+		return Mono.empty();
+	}
+
+}
