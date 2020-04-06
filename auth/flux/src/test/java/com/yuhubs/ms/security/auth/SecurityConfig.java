6467f37b760a2c64763569a28e01c19f42ec58d4
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-04-06 12:28:03 -0400

    Add the SecurityConfig for auth test

---
+++ auth/flux/src/test/java/com/yuhubs/ms/security/auth/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.yuhubs.ms.security.auth;
+
+import com.yuhubs.ms.auth.mock.MockAuthUserService;
+import com.yuhubs.ms.security.auth.web.AuthConfigurationSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@EnableWebFluxSecurity
+@Configuration
+public class SecurityConfig extends AuthConfigurationSupport {
+
+	@Autowired
+	private MockAuthUserService authUserService;
+
+
+	@Override
+	protected AuthUserService getAuthUserService() {
+		return this.authUserService;
+	}
+
+}
