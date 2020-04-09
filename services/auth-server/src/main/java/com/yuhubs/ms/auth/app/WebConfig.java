093bb8ccc7d4298c7a65b048fce9f5d51ea7dbd4
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-04-09 18:00:52 -0400

    Config web exception handler

--- services/auth-server/src/main/java/com/yuhubs/ms/auth/app/WebConfig.java
+++ services/auth-server/src/main/java/com/yuhubs/ms/auth/app/WebConfig.java
@@ -1,9 +1,15 @@
 package com.yuhubs.ms.auth.app;
 
 import com.yuhubs.ms.auth.web.WebConfigSupport;
+import com.yuhubs.ms.web.RestExceptionHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
 import org.springframework.web.reactive.config.ResourceHandlerRegistry;
+import org.springframework.web.reactive.function.server.ServerRequest;
+
+import java.net.URI;
 
 @Configuration
 public class WebConfig extends WebConfigSupport {
@@ -14,12 +20,49 @@ public class WebConfig extends WebConfigSupport {
 	@Value("${spring.resources.static-locations}")
 	private String resourceLocations;
 
+
+	@Value("${yuhubs.ms.app.rest-api.location}")
+	private String restApiLocation;
+
+	@Value("${yuhubs.ms.app.not-found-redirect.uri:/public/index.html}")
+	private String redirectURI;
+
+
 	@Override
 	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
 		if (staticResourcesEnabled) {
 			registry.addResourceHandler("/public/**")
 					.addResourceLocations(resourceLocations);
 		}
+	}
+
+
+	@Override
+	protected RestExceptionHandler createRestExceptionHandler() {
+		return new ExceptionHandler(this);
+	}
+
+
+	private static final class ExceptionHandler extends RestExceptionHandler {
+
+		private final WebConfig config;
+
+
+		private ExceptionHandler(WebConfig config) {
+			this.config = config;
+		}
+
+		@Override
+		protected URI getNotFoundRedirectUri(ServerRequest request,
+											 String httpMethod,
+											 String requestURL) {
+			PathMatcher pathMatcher = new AntPathMatcher();
+			if (!pathMatcher.match(config.restApiLocation, requestURL)) {
+				return URI.create(config.redirectURI);
+			}
+			return null;
+		}
+
 	}
 
 }
