a5b55bced30fb7b61835e5832b7d1abb2b4e478a
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-04-10 09:37:38 -0400

    Add the MailConfig

---
+++ services/auth-server/src/main/java/com/yuhubs/ms/auth/app/MailConfig.java
@@ -0,0 +1,69 @@
+package com.yuhubs.ms.auth.app;
+
+import com.yuhubs.ms.mail.MailConfigurationSupport;
+import com.yuhubs.ms.mail.MailTemplateManager;
+import com.yuhubs.ms.mail.MailTemplateSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.system.ApplicationHome;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
+@Configuration
+@Profile("dev")
+public class MailConfig extends MailConfigurationSupport {
+
+	@Component
+	@Profile("dev")
+	public static class PreloadTemplatesTask implements CommandLineRunner {
+
+		@Autowired
+		private MailTemplateManager templateManager;
+
+		@Autowired
+		private MailTemplateSupport templateSupport;
+
+
+		@Override
+		public void run(String... args) throws Exception {
+			templateManager.preloadTemplates(templateSupport);
+		}
+
+	}
+
+
+	private final File homeDir = new ApplicationHome(Application.class).getDir();
+
+
+	@Autowired
+	private ResourceLoader resourceLoader;
+
+	@Autowired
+	private MailTemplateManager mailTemplateManager;
+
+
+	@Override
+	protected ResourceLoader getResourceLoader() {
+		return this.resourceLoader;
+	}
+
+	@Override
+	protected MailTemplateManager getMailTemplateManager() {
+		return this.mailTemplateManager;
+	}
+
+	@Override
+	protected String getRealPath(String path) {
+		File file = new File(homeDir, path);
+		file = file.toPath().normalize().toFile();
+		if (file.exists()) {
+			return file.getAbsolutePath();
+		}
+		return super.getRealPath(path);
+	}
+
+}
