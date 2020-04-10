96f9656a71b4784a728cedd18e09843d1109f0f4
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-04-10 11:20:11 -0400

    Add the AuthEventHandler

---
+++ services/auth-server/src/main/java/com/yuhubs/ms/auth/app/AuthEventHandler.java
@@ -0,0 +1,47 @@
+package com.yuhubs.ms.auth.app;
+
+import com.yuhubs.ms.auth.mail.AuthEmailSender;
+import com.yuhubs.ms.mail.MailTemplateSupport;
+import com.yuhubs.ms.security.auth.AuthEventListenerSupport;
+import com.yuhubs.ms.security.auth.event.data.ConfirmEmailData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.MailException;
+
+@Configuration
+public class AuthEventHandler extends AuthEventListenerSupport {
+
+	private final AuthEmailSender emailSender;
+
+	@Autowired
+	private MailTemplateSupport mailTemplateSupport;
+
+
+	public AuthEventHandler() {
+		this.emailSender = new AuthEmailSender();
+	}
+
+
+	@Override
+	protected void handleConfirmEmail(ConfirmEmailData eventData) {
+		String templateId;
+
+		switch (eventData.getType()) {
+			case RESET_PASSWORD:
+				templateId = AuthTemplates.RESET_PASSWORD_ID;
+				break;
+			case VERIFY_EMAIL:
+			default:
+				templateId = AuthTemplates.VERIFY_EMAIL_ID;
+				break;
+		}
+
+		try {
+			this.emailSender.sendEmail(this.mailTemplateSupport,
+					templateId, eventData);
+		} catch (MailException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
