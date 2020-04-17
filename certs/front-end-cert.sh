6ff0aa6ecafff2949a4cd8395d20ed2fc8ebfe80
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-04-17 15:37:07 -0400

    Define and generate front end certs

---
+++ certs/front-end-cert.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)
+
+
+export OUTPUT_DIR=./deploy
+export OUTPUT_CA_DIR=$OUTPUT_DIR/ca
+
+export CA_NAME=yuhubs-ecom
+
+export HOST_NAME=front-end
+
+
+export OUTPUT_HOST_DIR=$OUTPUT_DIR/$HOST_NAME
+
+mkdir -p $OUTPUT_HOST_DIR
+
+
+if [ ! -f "$OUTPUT_CA_DIR/$CA_NAME.pem" ]; then
+  ./build-ca.sh
+fi
+
+
+if [ ! -f "$OUTPUT_HOST_DIR/$HOST_NAME.pem" ]; then
+
+# Build Server Cert
+echo "--- Build Server Cert ---"
+cfssl gencert -ca=$OUTPUT_CA_DIR/$CA_NAME.pem -ca-key=$OUTPUT_CA_DIR/$CA_NAME-key.pem \
+  -config=./ca/ca-config.json -profile="server" \
+  ./hosts/$HOST_NAME-csr.json | \
+cfssljson -bare $OUTPUT_HOST_DIR/$HOST_NAME
+
+fi
+
+# Verify the Server Cert
+echo "--- Verify the Server Cert ---"
+openssl verify -CAfile $OUTPUT_DIR/$CA_NAME-ca.crt \
+  $OUTPUT_HOST_DIR/$HOST_NAME.pem
