115faaea918087e5d658487c02a18de2787accd5
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-04-17 17:19:35 -0400

    Config nginx https server

--- front-end/deploy/docker-bin/run.sh
+++ front-end/deploy/docker-bin/run.sh
@@ -8,6 +8,6 @@ echo "export YUHUBS_MS_HTML_LOCATION=\"${YUHUBS_MS_HTML_LOCATION}\""
 echo ""
 
 docker run -d --name ecom-front-end \
-  --network=ecom-backend --net-alias=front-end -p 8080:80 \
+  --network=ecom-backend --net-alias=front-end -p 8080:80 -p 8443:443 \
   -v "${YUHUBS_MS_HTML_LOCATION}":/usr/share/nginx/html:ro \
   com.yuhubs.ecom/front-end:0.0.1
