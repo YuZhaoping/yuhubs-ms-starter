71641070db6095928897a3eacf7d05a19b309212
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-04-27 18:57:47 -0400

    Setup embedded-redis for test

--- commons/data/redis/src/test/java/com/yuhubs/ms/redis/ApplicationTestConfig.java
+++ commons/data/redis/src/test/java/com/yuhubs/ms/redis/ApplicationTestConfig.java
@@ -1,7 +1,14 @@
 package com.yuhubs.ms.redis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Component;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.IOException;
 
 @Configuration
 @Import({
@@ -9,4 +16,28 @@ import org.springframework.context.annotation.Import;
 		RedisClusterConfig.class
 })
 public class ApplicationTestConfig {
+
+
+	@Component
+	public static class EmbededRedis {
+
+		@Value("${spring.redis.port}")
+		private int redisPort;
+
+		private RedisServer redisServer;
+
+
+		@PostConstruct
+		public void startRedis() throws IOException {
+			redisServer = new RedisServer(redisPort);
+			redisServer.start();
+		}
+
+		@PreDestroy
+		public void stopRedis() {
+			redisServer.stop();
+		}
+
+	}
+
 }
