bdb0d3e6dbd277fdc91915420b31c1c6855d108a
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-04-28 18:32:26 -0400

    Setup the embedded redis server for test

--- auth/data/redis/src/test/java/com/yuhubs/ms/auth/redis/ConfiguredTestBase.java
+++ auth/data/redis/src/test/java/com/yuhubs/ms/auth/redis/ConfiguredTestBase.java
@@ -1,12 +1,16 @@
 package com.yuhubs.ms.auth.redis;
 
 import org.junit.runner.RunWith;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ContextConfiguration(classes = ApplicationTestConfig.class)
+@TestPropertySource(locations="classpath:redis.properties")
+@EnableConfigurationProperties
 public class ConfiguredTestBase {
 }
