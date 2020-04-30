9d83499777dbd37f9e43892bca75b3877aa07eef
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-04-30 11:27:25 -0400

    Polish

--- auth/data/redis/src/main/java/com/yuhubs/ms/auth/redis/RedisAuthUserServiceBase.java
+++ auth/data/redis/src/main/java/com/yuhubs/ms/auth/redis/RedisAuthUserServiceBase.java
@@ -4,7 +4,6 @@ public class RedisAuthUserServiceBase {
 
 	private static final String USER_ID_KEY_PREFIX    = "auth-user:id:";
 	private static final String USER_NAME_KEY_PREFIX  = "auth-user:name:";
-	private static final String USER_EMAIL_KEY_PREFIX = "auth-user:email:";
 
 
 	protected static String userIdToKey(Long userId) {
@@ -17,18 +16,6 @@ public class RedisAuthUserServiceBase {
 
 	protected static String userNameToKey(String username) {
 		return USER_NAME_KEY_PREFIX + username;
-	}
-
-	protected static String userNameFromKey(String key) {
-		return key.substring(USER_NAME_KEY_PREFIX.length());
-	}
-
-	protected static String userEmailToKey(String email) {
-		return USER_EMAIL_KEY_PREFIX + email;
-	}
-
-	protected static String userEmailFromKey(String key) {
-		return key.substring(USER_EMAIL_KEY_PREFIX.length());
 	}
 
 }
