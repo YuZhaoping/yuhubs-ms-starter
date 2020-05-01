ad51e9c02711077d3f3fbdef301c7deea4346db9
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-05-01 10:22:52 -0400

    Mark Serializable for AuthUserProfile

--- auth/core/src/main/java/com/yuhubs/ms/security/auth/AuthUserProfile.java
+++ auth/core/src/main/java/com/yuhubs/ms/security/auth/AuthUserProfile.java
@@ -1,6 +1,11 @@
 package com.yuhubs.ms.security.auth;
 
-public class AuthUserProfile {
+import java.io.Serializable;
+
+public class AuthUserProfile implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
 
 	private String name;
 
