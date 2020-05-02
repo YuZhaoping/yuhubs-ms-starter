5b8c6cdad826ad7241f006f2444cbff7505b1867
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-05-02 17:21:40 -0400

    Add the AuthUserGroupRoleSupport

---
+++ auth/model/src/main/java/com/yuhubs/ms/auth/model/AuthUserGroupRoleSupport.java
@@ -0,0 +1,87 @@
+package com.yuhubs.ms.auth.model;
+
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class AuthUserGroupRoleSupport {
+
+	public final String getRolesByGroups(String groups) {
+		final StringBuilder result = new StringBuilder();
+
+		rolesStreamByGroups(groups).forEach(role -> result.append(',').append(role));
+
+		return (result.length() > 0) ? result.substring(1) : "";
+	}
+
+	public final String getPermissionsByGroups(String groups) {
+		final StringBuilder result = new StringBuilder();
+
+		permissionsStreamByRoles(rolesStreamByGroups(groups))
+				.forEach(permission -> result.append(',').append(permission));
+
+		return (result.length() > 0) ? result.substring(1) : "";
+	}
+
+	public final String getPermissionsByRoles(String roles) {
+		final StringBuilder result = new StringBuilder();
+
+		permissionsStreamByRoles(rolesStream(roles))
+				.forEach(permission -> result.append(',').append(permission));
+
+		return (result.length() > 0) ? result.substring(1) : "";
+	}
+
+
+	private final Stream<String> rolesStreamByGroups(String groups) {
+		return streamOfString(groups)
+				.map(this::getUserGroupRoles)
+				.flatMap(roles -> rolesStream(roles))
+				.distinct();
+	}
+
+	private final Stream<String> permissionsStreamByRoles(Stream<String> rolesStream) {
+		return rolesStream
+				.map(this::getRolePermissions)
+				.flatMap(permissions -> permissionsStream(permissions))
+				.distinct();
+	}
+
+	private static Stream<String> rolesStream(String roles) {
+		return streamOfString(roles);
+	}
+
+	private static Stream<String> permissionsStream(String permissions) {
+		return streamOfString(permissions);
+	}
+
+	private static Stream<String> streamOfString(String str) {
+		return Arrays.stream(str.split(","))
+				.map(String::trim)
+				.filter(StringUtils::hasText);
+	}
+
+
+	public String getDefaultUserGroups() {
+		return "USER";
+	}
+
+	public String getAllUserGroups() {
+		return "ROOT, ADMIN, USER";
+	}
+
+	public String getUserGroupRoles(String userGroup) {
+		return userGroup;
+	}
+
+	public String getRolePermissions(String role) {
+		switch (role) {
+			case "ROOT": return "GRANT-ROLE";
+			case "ADMIN": return "ADMIN-USER, USE-*";
+			case "USER": return "USE-*";
+		}
+		return null;
+	}
+
+}
