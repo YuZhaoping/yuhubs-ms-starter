920d4899fe5e00a61a5fea25841ff1e9053727c4
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-06-16 18:07:35 -0400

    Add install init kubelet script

---
+++ k8s/provisions/Services/_init/_kubelet.sh
@@ -0,0 +1,31 @@
+
+kube_node_src_dir="$DEPLOY_HOME/${node_name}/kube/kubelet"
+
+if [ -d "$kube_node_src_dir" ]; then
+echo "--- init: [${node_name}] kubelet ---"
+
+
+kube_common_src_dir="$DEPLOY_HOME/kube/kubelet"
+
+cp_systemd_service "$kube_common_src_dir" "kubelet.service"
+mk_service_workdir "$kube_common_src_dir/kubelet.service"
+
+
+kube_env_dest_dir="${K8S_KUBE_ETC_HOME:-/etc/kubernetes}"
+kube_env_dest_dir=$(echo "${_ROOT}$kube_env_dest_dir" | sed 's/\\//g')
+
+mkdir -p "$kube_env_dest_dir"
+
+
+cp -vf $kube_common_src_dir/*.env "$kube_env_dest_dir/"
+cp -vf $kube_common_src_dir/*.conf "$kube_env_dest_dir/"
+
+#cp -vf $kube_node_src_dir/*.env "$kube_env_dest_dir/"
+cp -vf $kube_node_src_dir/*.json "$kube_env_dest_dir/"
+
+
+if [ "n$_DRY_RUN" = "n" ]; then
+  chown -R $RUN_USER:$RUN_USER "$kube_env_dest_dir"
+fi
+
+fi
