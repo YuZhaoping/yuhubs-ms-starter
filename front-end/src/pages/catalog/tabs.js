9e6cae5c50d211440698200cc0e58546d78674fe
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-07-02 13:12:23 -0400

    Polish

--- front-end/src/pages/catalog/tabs.js
+++ front-end/src/pages/catalog/tabs.js
@@ -1,7 +1,7 @@
 
 const tabs = [
-  { value: 'common-categories', label: 'Common-categories', path: '/app/catalog/categories/common' },
-  { value: 'customized-categories', label: 'Customized-categories', path: '/app/catalog/categories/customized' },
+  { value: 'common-categories', label: 'Common-Categories', path: '/app/catalog/categories/common' },
+  { value: 'customized-categories', label: 'Customized-Categories', path: '/app/catalog/categories/customized' },
   { value: 'products', label: 'Products', path: '/app/catalog/products' }
 ];
 
