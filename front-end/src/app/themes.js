7dc92913e2b4fd6814e4d333193786474c71523a
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-07-02 07:59:28 -0400

    Define default theme

--- front-end/src/app/themes.js
+++ front-end/src/app/themes.js
@@ -1,25 +1,11 @@
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
-import blue from '@material-ui/core/colors/blue';
-import deepOrange from '@material-ui/core/colors/deepOrange';
 import grey from '@material-ui/core/colors/grey';
 
 
 const variants = [
   {
-    name: "Blue",
-    palette: {
-      primary: {
-        main: blue[700],
-        dark: blue[800],
-        contrastText: "#FFF"
-      },
-      secondary: {
-        main: deepOrange[700],
-        dark: deepOrange[800],
-        contrastText: "#FFF"
-      }
-    },
+    name: "Default",
     sidebar: {
       background: grey[50],
       color: grey[700]
@@ -38,9 +24,6 @@ const variants = [
 const theme = variant => {
   const muiTheme = createMuiTheme(
     {
-      palette: {
-        ...variant.palette
-      },
       spacing: 4,
       props: {
         MuiButtonBase: {
