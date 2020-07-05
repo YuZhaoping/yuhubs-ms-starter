af3473480829220545b04c8cac37955315d6cec6
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-07-05 17:32:02 -0400

    Refactor the PagesLayout component

--- front-end/src/pages/PagesLayout.js
+++ front-end/src/pages/PagesLayout.js
@@ -1,47 +1,5 @@
-import React from 'react';
+import async from 'Components/Async';
 
-
-import makeStyles from '@material-ui/core/styles/makeStyles';
-
-import PageTitleBar from 'Pages/components/PageTitleBar';
-
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    overflow: 'hidden',
-    flexDirection: 'column'
-  },
-
-  pageHeader: {
-    backgroundColor: theme.menubar.background,
-    color: theme.menubar.color,
-    padding: theme.spacing(0, 2)
-  },
-
-  pageContent: {
-    flexGrow: 1,
-    marginLeft: theme.spacing(2)
-  }
-}));
-
-
-const PagesLayout = (props) => {
-  const { children, ...rest } = props;
-
-  const classes = useStyles();
-
-  return (
-    <div className={ classes.root } >
-      <div className={ classes.pageHeader } >
-        <PageTitleBar { ...rest } />
-      </div>
-      <div className={ classes.pageContent } >
-        { children }
-      </div>
-    </div>
-  );
-};
-
+const PagesLayout = async(() => import(/* webpackChunkName: "pages-layout" */ './components/PagesLayout'));
 
 export default PagesLayout;
---
+++ front-end/src/pages/components/PagesLayout.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+import PageTitleBar from './PageTitleBar';
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    overflow: 'hidden',
+    flexDirection: 'column'
+  },
+
+  pageHeader: {
+    backgroundColor: theme.menubar.background,
+    color: theme.menubar.color,
+    padding: theme.spacing(0, 2)
+  },
+
+  pageContent: {
+    flexGrow: 1,
+    marginLeft: theme.spacing(2)
+  }
+}));
+
+
+const PagesLayout = (props) => {
+  const { children, ...rest } = props;
+
+  const classes = useStyles();
+
+  return (
+    <div className={ classes.root } >
+      <div className={ classes.pageHeader } >
+        <PageTitleBar { ...rest } />
+      </div>
+      <div className={ classes.pageContent } >
+        { children }
+      </div>
+    </div>
+  );
+};
+
+
+export default PagesLayout;
