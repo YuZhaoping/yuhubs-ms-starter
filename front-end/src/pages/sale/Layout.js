6e000116796d6ad27aeb4030e7650e4debcae326
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-07-05 12:26:31 -0400

    Setup PageSidebar for the sale layout

--- front-end/src/pages/sale/Layout.js
+++ front-end/src/pages/sale/Layout.js
@@ -5,15 +5,49 @@ import PagesLayout from 'Pages/PagesLayout';
 
 import tabs from './tabs';
 
+import PageSidebar from 'Pages/components/PageSidebar';
+
+
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    overflow: 'hidden',
+    flexDirection: 'column'
+  },
+
+  sidebarAnchor: {
+    height: 0,
+    flexShrink: 0,
+    flexGrow: 0
+  },
+
+  saleContent: {
+    flexGrow: 1
+  }
+}));
+
 
 const title = 'Sale';
 
 const Layout = (props) => {
-  const { children, ...rest } = props;
+  const { sidebarComponent, children, ...rest } = props;
+
+  const classes = useStyles();
 
   return (
     <PagesLayout title={ title } tabs={ tabs } { ...rest } >
-      { children }
+      <div className={ classes.root } >
+        <PageSidebar
+          className={ classes.sidebarAnchor }
+          sidebarComponent={ sidebarComponent }
+          { ...rest }
+        />
+        <div className={ classes.saleContent } >
+          { children }
+        </div>
+      </div>
     </PagesLayout>
   );
 };
