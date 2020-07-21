09c8e4fd1ab14fa6d94cc37a47b94b2aa98fc738
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-07-21 11:01:11 -0400

    Update build-all.sh to support more build targets

--- build-all.sh
+++ build-all.sh
@@ -3,6 +3,21 @@
 cd $(dirname $0)
 
 
+build_target="apps"
+if [ "$#" -gt 0 ]; then
+  build_target=$1
+fi
+
+case "${build_target}" in
+  "libs" | "svcs" | "apps")
+    ;;
+  *)
+    echo "Usage: build-all.sh [libs | svcs | apps]";
+    exit 1;
+    ;;
+esac
+
+
 mvn -N install
 
 ./commons/build-all.sh
@@ -10,7 +25,18 @@ mvn -N install
 ./auth/build-all.sh
 
 ./services/build-pom.sh
+
+
+if [ "x$build_target" = "xlibs" ]; then
+  exit 0
+fi
+
 ./services/auth-server/cmd/build.sh
 ./services/catalog/cmd/build.sh
+
+
+if [ "x$build_target" = "xsvcs" ]; then
+  exit 0
+fi
 
 ./front-end/cmd/build.sh
